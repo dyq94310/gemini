5acff381cbebceb069ef034ed54057ed039283d4
dyq <dyq94310@foxmail.com>
2024-06-14 17:10:33 +0800

    append content  to file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import google.generativeai as genai
 import requests
 import fitz 
-from datetime import datetime
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 def get_models():
   for m in genai.list_models():
@@ -10,14 +11,14 @@ def get_models():
       print(m.name)
 
 def generate_content():
-  generate_content('hell,what is diff from gemini-1.5-pro gemini-1.5-flase')
+  return generate_content('hell,what is diff from gemini-1.5-pro gemini-1.5-flase')
 
 def generate_content_with_prompt(prompt,content):
   if content is None:
     print("content is None")
     return
   tmp = prompt + content
-  generate_content(tmp)
+  return generate_content(tmp)
 
 def generate_content(content):
   if content is None:
@@ -26,7 +27,7 @@ def generate_content(content):
   model_name = os.getenv("MODEL_NAME",'gemini-1.5-flash')
   model = genai.GenerativeModel(model_name)
   response = model.generate_content(content)
-  print(response.text)
+  return response.text
 
 def download_pdf(url,filename):
   response = requests.get(url)
@@ -50,15 +51,26 @@ def get_pfd_text(pdf_path):
       all_text += page.get_text()
   return all_text
 
-prompt = os.getenv('PROMPT')
+def append_to_file(filename,content):
+  with open(filename, "a") as file:
+    # Write the content to the file
+    file.write(content + "\n")
+
+
+
+load_dotenv() 
+
+prompt = os.getenv('US_PROMPT')
 print(f"prompt = {prompt}")
 
+today  = datetime.today()
+yesterday = today - timedelta(days=1)
+today_str = os.getenv("US_SPRCIAL_DATE",yesterday.strftime('%Y%m%d'))
 
-today =  datetime.today()
-today_str = os.getenv("SPRCIAL_DATE",today.strftime('%Y%m%d'))
+save_file = os.getenv("SAVE_FILE")
+url = os.getenv("US_URL")
 
-url = os.getenv("URL")
-items = os.getenv("FILES").split(",")
+items = os.getenv("US_FILES").split(",")
 for item in items:
   full_path= f"{url}{item}{today_str}.pdf"
   print(f"download {full_path}")
@@ -66,4 +78,7 @@ for item in items:
   filename = f"{item}.pdf"
   download_pdf(full_path,filename)
   pdf_context = get_pfd_text(filename)
-  generate_content_with_prompt(prompt=prompt,content=pdf_context)
+  gen_content = generate_content_with_prompt(prompt=prompt,content=pdf_context)
+  
+  save_contrent = f"*****{today_str}{filename} ***** \n {gen_content} \n *****{today_str}***** \n "
+  append_to_file(save_file,save_contrent)
